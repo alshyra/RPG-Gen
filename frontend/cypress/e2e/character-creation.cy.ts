ef4dab667c9112bdefe2b624a9a9ef844cae2f3a
Alshyra <antoine.savajols@gmail.com>
2025-11-13 16:51:08 +0100

    fix e2e

--- frontend/cypress/e2e/character-creation.cy.ts
+++ frontend/cypress/e2e/character-creation.cy.ts
@@ -30,11 +30,11 @@ describe("Character Creation", () => {
     // Should see Race & Class step
     cy.contains("Race & Classe").should("be.visible");
 
-    // Select a race (just click the first one)
+    // Select a race (click the first race button)
     cy.contains("Humain").click();
 
-    // Select a class (just click the first one)
-    cy.contains("Barbarian").click();
+    // Select a class from the select dropdown
+    cy.get("select").select("Barbarian");
 
     // Go to next step
     cy.contains("button", "Suivant").click();
