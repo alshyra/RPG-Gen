a46579b1dc3add60b464d34fd05f7bffcfab12cb
Antoine Savajols <a.savajols.ext@sinari.com>
2025-11-17 18:09:26 +0100

    fix backend

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -57,7 +57,7 @@ const bootstrap = async () => {
   validateEnv(app);
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, forbidNonWhitelisted: false }));
   setupSwagger(app);
-  const port = process.env.PORT ? parseInt(process.env.PORT, 10) : 3001;
+  const port = process.env.SERVER_PORT ? parseInt(process.env.SERVER_PORT, 10) : 3001;
   await app.listen(port, "0.0.0.0");
   await logApplicationStartup(app, port);
 }
