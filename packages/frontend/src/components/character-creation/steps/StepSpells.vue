ff37ec982b12ace9d9eebdcbd89104bf45a222d8
Alshyra <antoine.savajols@gmail.com>
2025-11-25 22:14:00 +0100

    fix e2e tests

--- packages/frontend/src/components/character-creation/steps/StepSpells.vue
+++ packages/frontend/src/components/character-creation/steps/StepSpells.vue
@@ -59,7 +59,10 @@ const characterStore = useCharacterStore();
 const { currentCharacter } = storeToRefs(characterStore);
 
 const primaryClass = computed(() => currentCharacter.value?.classes?.[0]?.name ?? '');
-const availableSpells = computed(() => DnDRulesService.getAvailableSpellsForClass(primaryClass.value));
+const availableSpells = computed(() => DnDRulesService
+  .getAvailableSpellsForClass(primaryClass.value)
+  .filter(s => s.level <= 1),
+);
 
 const spellIsSelected = (name: string) => (currentCharacter.value?.spells || []).some(s => s.name === name);
 
