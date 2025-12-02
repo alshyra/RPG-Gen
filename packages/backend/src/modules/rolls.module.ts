de6bf98fca5b4c94a9986a429c0a23c1d35ff0bc
Alshyra <antoine.savajols@gmail.com>
2025-12-02 13:12:28 +0100

    fix(rolls): import character & combat modules, provide GameInstructionProcessor

--- packages/backend/src/modules/rolls.module.ts
+++ packages/backend/src/modules/rolls.module.ts
@@ -1,7 +1,13 @@
 import { Module } from '@nestjs/common';
 import { RollsController } from '../controllers/rolls.controller.js';
+import { CharacterModule } from './character.module.js';
+import { CombatModule } from './combat.module.js';
+import { GameInstructionProcessor } from '../domain/chat/game-instruction.processor.js';
 
 @Module({
+  imports: [CharacterModule, CombatModule],
   controllers: [RollsController],
+  providers: [GameInstructionProcessor],
+  exports: [GameInstructionProcessor],
 })
 export class RollsModule {}
